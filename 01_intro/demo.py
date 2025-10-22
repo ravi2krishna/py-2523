2c58dc538d004071874871ddddd822f90ab28cd0
ravi2krishna <ravi2krishna@gmail.com>
2025-10-22 07:57:46 +0530

    22nd Oct - Identifiers & Variables

--- 01_intro/demo.py
+++ 01_intro/demo.py
@@ -1,2 +1,38 @@
-a = 10
-print(a)
+# This is a comment, python interpreter will ignore this line 
+# function -> does a task 
+# function(arguments)
+# we have print() -> prints on standard output 
+print(10)
+print(4.2)
+# print(good morning) # SyntaxError: invalid syntax.
+print("good morning")
+print('good morning')
+
+# Identifiers 
+today = "wednesday"
+print(today)
+
+# 2day = "wednesday" # SyntaxError: invalid decimal literal
+# print(2day)
+
+day2moro = "thursday"
+print(day2moro)
+
+_2moro = "thu"
+print(_2moro)
+
+# $today = "wed" # SyntaxError: invalid syntax
+# print($today)
+
+# Improper way of using identifiers
+x = "Ravi" # name
+y = 25 # age
+z = 9.5 # gpa 
+
+# Proper way of using identifiers
+student_name = "Ravi"
+student_age = 25
+student_gpa = 9.5
+
+
+
---
+++ 02_variables/demo.py
@@ -0,0 +1,16 @@
+# Variables 
+# variable_name = value 
+
+# Assign Data (Storing Data)
+student_name = "Ravi" # string 
+student_age = 25 # int 
+student_gpa = 9.5 # float 
+student_passed = True # boolean True/False
+student_unknown_data = None # represents no value 
+
+# Retrieve Data -> print()
+print(student_name)
+print(student_age)
+print(student_gpa)
+print(student_passed)
+print(student_unknown_data)
