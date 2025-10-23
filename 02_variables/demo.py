faffe09074b0a5f0445015bc7cb95749a4031d12
ravi2krishna <ravi2krishna@gmail.com>
2025-10-23 08:01:56 +0530

    23rd Oct - Variables Output

--- 02_variables/demo.py
+++ 02_variables/demo.py
@@ -14,3 +14,51 @@ print(student_age)
 print(student_gpa)
 print(student_passed)
 print(student_unknown_data)
+
+# Retrieve Data with statements -> concatenation
+# print("Student Name" student_name) # SyntaxError: invalid syntax
+print("Student Name: " +student_name)
+# print("Student Age: " +student_age) # TypeError: can only concatenate str (not "int") to str
+print("Student Age: ", student_age)
+print("Student GPA: ",student_gpa)
+print("Student Passed: " ,student_passed)
+print("Student Unknown: ",student_unknown_data)
+print("Student Name: " ,student_name)
+
+print("=================")
+
+# Get identity / memory address 
+print(id(student_name))
+print(id(student_age))
+print(id(student_gpa))
+print(id(student_passed))
+print(id(student_unknown_data))
+
+print("=================")
+
+# Memory Model Of Python
+value_x = 10
+print(id(value_x))
+
+value_y = 20
+print(id(value_y))
+
+value_z = 10
+print(id(value_z))
+
+print("=================")
+
+list_data_x = [10,20,30,40,50]
+print(id(list_data_x))
+
+list_data_z = [10,20,30,40,50]
+print(id(list_data_z))
+
+print("=================")
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_gpa))
+print(type(student_passed))
+print(type(student_unknown_data))
+print(type(list_data_z))
---
+++ 03_operators/demo.py
@@ -0,0 +1,10 @@
+# Operators 
+
+# Arithmetic Operators -> Mathematical Calculations
+num1 = 10
+num2 = 5
+
+print("The Sum of Two Numbers: ",num1+num2)
+print("The Sum of {num1} and {num2} is {num1+num2}") # without f 
+print(f"The Sum of {num1} and {num2} is {num1+num2}")# with f 
+
