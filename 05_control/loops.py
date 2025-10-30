780aad9d2f2b555ce8cfabe43623788eaad6ddf7
ravi2krishna <ravi2krishna@gmail.com>
2025-10-30 07:59:28 +0530

    30th Oct - Nested Loops

--- 05_control/loops.py
+++ 05_control/loops.py
@@ -82,3 +82,49 @@ for num in range(10, 0, -1):
 # Say Hi for 10 times
 for greet in range(10):
     print("Hi")
+
+# Nested While Loops 
+outer = 1
+while outer < 6:
+    inner = 1 
+    while inner < 6:
+        print(f"{outer} * {inner} = {outer * inner}")
+        inner+=1
+    outer+=1
+
+# simple read world use case -> ecommerce 
+colors = ["red","black","green"]
+sizes = ["XS","S","M","L","XL","XXL"]
+print(len(colors))
+print(len(sizes))
+# print("red XS")
+# print("red S")
+# print("red M")
+i = 0
+while i < 3:
+    j = 0
+    while j < 6:
+        print(colors[i] +" "+ sizes[j])
+        j+=1
+    i+=1
+     
+i = 0
+while i < len(colors):
+    j = 0
+    while j < len(sizes):
+        print(colors[i] +" "+ sizes[j])
+        j+=1
+    i+=1   
+    
+# Nested for loop
+for outer in range(1,4):
+    for inner in range(1,4):
+        print(f"{outer} * {inner} = {outer * inner}")
+
+# simple read world use case -> ecommerce 
+colors = ["red","black","green"]
+sizes = ["XS","S","M","L","XL","XXL"]
+
+for color in colors:
+    for size in sizes:
+        print(color, size)         
