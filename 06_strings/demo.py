9b346676164184c97ed56a991a525c097208b324
ravi2krishna <ravi2krishna@gmail.com>
2025-11-03 07:58:58 +0530

    3rd Nov - Strings In Python

--- 06_strings/demo.py
+++ 06_strings/demo.py
@@ -45,3 +45,83 @@ print(answer)
 question = "how are you ?"
 answer = '''i"m fine i'm fine'''
 print(answer)
+
+# Accessing Strings 
+text = "python"
+print(text)
+
+# Access characters using index 
+# Positive Index
+print(text[0])
+print(text[1])
+
+# Negative Index
+print(text[-1])
+print(text[-2])
+
+# print(text[10]) # IndexError: string index out of range
+
+# Access all characters 
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+print(text[5])
+
+# Access all characters 
+text = "python"
+for char in text:
+    print(char)
+
+# len() used to check the number of characters 
+print("Length Of String: ",len(text))
+
+# slicing -> string[start:end:step]
+text = "python"
+print(text[:])
+print(text[::])
+print(text[0:3:1]) # pyt
+print(text[1:3]) # yt
+print(text[0:5:2]) # pto
+
+         #    0  1  2  3  4  5   [Positive Indexing]
+        #     p  y  t  h  o  n
+        #    -6  -5 -4 -3 -2 -1  [Negative Indexing]
+
+print(text[-4:-1:1]) # tho
+print(text[-4:-1:-1]) # empty
+print(text[-4:-6:-1]) # ty
+
+# String Concatenation 
+s = "good"
+m = "morning"
+print(s+m)
+
+age = 30
+# print("My Age is: "+age) # TypeError: can only concatenate str (not "int") to str
+
+# Formatted String Literals (f-strings)
+print(f"My Age is: {age}")
+
+# String Repetition 
+laugh = "HaHaHa"
+print(laugh)
+
+hard_laugh = laugh * 10
+print(hard_laugh)
+
+# String Immutability 
+greet = "hello" # -> Hello
+print(greet)
+print(greet[0])
+# greet[0] = "H" # TypeError: 'str' object does not support item assignment
+list_greet = ['h','e','l','l','o']
+print(list_greet)
+print(list_greet[0])
+list_greet[0] = "H"
+print(list_greet)
+print(list_greet[0])
+
+# string methods
+print(dir(greet))
