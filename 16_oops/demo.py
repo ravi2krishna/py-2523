f44d1a6aefcefd228456e0fd9bd7e44352fcc8da
ravi2krishna <ravi2krishna@gmail.com>
2025-12-11 07:59:11 +0530

    11th Dec - OOP - Instance, Class & Static Members

--- 16_oops/demo.py
+++ 16_oops/demo.py
@@ -121,3 +121,145 @@ student_mike = Student("mike","mike@gmail.com")
 student_mike.showInfo()
 
 print("=" * 20)
+
+
+# Student -> Real World Entity 
+# Instance Members 
+class Student:
+    
+    # special method i.e constructor 
+    def __init__(self, student_name, student_email):
+        # instance variables 
+        # self.student_name & self.student_email
+        self.student_name = student_name
+        self.student_email = student_email
+    
+    # Instance Method 
+    def showInfo(self):
+        print("Showing Info") 
+        print("Student Name: ",self.student_name)
+        print("Student Email: ",self.student_email) # this is recommended style   
+                
+# Object Creation 
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.showInfo()
+
+student_john = Student("john","john@gmail.com")
+student_john.showInfo()
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.showInfo()
+
+print("=" * 20)
+
+# Student -> Real World Entity 
+# Class Members 
+class Student:
+    # class variable
+    institute_name = "Edify"
+    
+    # special method i.e constructor 
+    def __init__(self, student_name, student_email):
+        # instance variables 
+        # self.student_name & self.student_email
+        self.student_name = student_name
+        self.student_email = student_email
+    
+    # Instance Method 
+    def showInfo(self):
+        print("Showing Info") 
+        # accessing class variable 
+        print("Institute Name: ",Student.institute_name) # this is recommended style   
+        # print("Institute Name: ",self.institute_name) # this is not recommended style   
+        print("Student Name: ",self.student_name)
+        print("Student Email: ",self.student_email) # this is recommended style   
+    
+    # Class Method 
+    @classmethod
+    def change_institute_name(cls,new_name):
+        cls.institute_name = new_name
+              
+# Object Creation 
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.showInfo()
+
+student_john = Student("john","john@gmail.com")
+student_john.showInfo()
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.showInfo()
+# student_mike.change_institute_name("Lync") # this is not recommended style   
+Student.change_institute_name("Lync") # this is recommended style   
+
+# Object Creation 
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.showInfo()
+
+student_john = Student("john","john@gmail.com")
+student_john.showInfo()
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.showInfo()
+
+print("=" * 20)
+
+# Student -> Real World Entity 
+# Instances Members, Class Members & Static Members 
+class Student:
+    # class variable
+    institute_name = "Edify"
+    
+    # special method i.e constructor 
+    def __init__(self, student_name, student_email):
+        # instance variables 
+        # self.student_name & self.student_email
+        self.student_name = student_name
+        self.student_email = student_email
+    
+    # Instance Method 
+    def showInfo(self):
+        print("Showing Info") 
+        # accessing class variable 
+        print("Institute Name: ",Student.institute_name) # this is recommended style   
+        # print("Institute Name: ",self.institute_name) # this is not recommended style   
+        print("Student Name: ",self.student_name)
+        print("Student Email: ",self.student_email) # this is recommended style   
+    
+    # Class Method 
+    @classmethod
+    def change_institute_name(cls,new_name):
+        cls.institute_name = new_name
+        
+    # Static Method
+    @staticmethod
+    def validate_email(email):
+        return "@" in email and "." in email # T and T -> T
+              
+# Object Creation 
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.showInfo()
+
+student_john = Student("john","john@gmail.com")
+student_john.showInfo()
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.showInfo()
+# student_mike.change_institute_name("Lync") # this is not recommended style   
+Student.change_institute_name("Lync") # this is recommended style   
+
+# Object Creation 
+student_ravi = Student("ravi","ravi@gmail.com")
+student_ravi.showInfo()
+
+student_john = Student("john","john@gmail.com")
+student_john.showInfo()
+
+student_mike = Student("mike","mike@gmail.com")
+student_mike.showInfo()
+
+# Validate Email -> calling static method 
+print(Student.validate_email("ravi"))
+print(Student.validate_email("ravi@gmailcom"))
+print(Student.validate_email("ravi@gmail.com"))
+
+print("=" * 20)
